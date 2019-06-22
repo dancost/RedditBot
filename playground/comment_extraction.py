913e0d2ca08eace08aeca857950ee1fce216d7d5
Dan <dan.costache@smarttra.de>
2019-06-22 22:58:32 +0200

    multiple extraction examples

--- playground/comment_extraction.py
+++ playground/comment_extraction.py
@@ -1,5 +1,7 @@
 import praw
+from praw.models import MoreComments
 import configparser
+
 
 config = configparser.ConfigParser()
 config.read('./utils/config.ini')
@@ -18,3 +20,18 @@ reddit = praw.Reddit(client_id=client_id,
                      username=username,
                      password=password)
 
+submission = reddit.submission(id='3g1jfi')
+
+for top_level_comment in submission.comments:
+    if isinstance(top_level_comment, MoreComments):
+        continue
+    print(top_level_comment.body)
+
+submission.comments.replace_more(limit=None)
+
+for top_level_comment in submission.comments:
+    for second_level_comment in top_level_comment.replies:
+        print(second_level_comment.body)
+
+for comment in submission.comments.list():
+    print(comment.body)
