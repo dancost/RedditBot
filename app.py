89ccc3507fbaafcd4dd70b6aa1e1e07a33725812
Dan <dan.costache@smarttra.de>
2019-06-23 03:44:39 +0200

    fix post request

--- app.py
+++ app.py
@@ -41,7 +41,6 @@ def messaging_events(payload):
             yield event["sender"]["id"], event["message"]["text"]
         else:
             yield event["sender"]["id"], "Can't echo this!"
-            break
 
 
 def send_message(token, recipient, text):
