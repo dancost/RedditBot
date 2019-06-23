9afca4110e5e7fe84c6078e07a3a68122c878ad4
Dan <dan.costache@smarttra.de>
2019-06-23 02:59:52 +0200

    update return error

--- app.py
+++ app.py
@@ -8,18 +8,21 @@ app = Flask(__name__)
 
 # Facebook Page Access Token
 PAT = os.environ.get('PAT')
-fb_v_token = os.environ.get('fbtoken')
+
 
 
 @app.route('/', methods=['GET'])
 def handle_verification():
-    print("Handling verification")
-    if request.args.get('hub.verify_token', '') == fb_v_token:
-        print("Verification successful")
-        return request.args.get('hub.challenge', '')
-    else:
-        print("Verification failed")
-        return "Error invalid token!"
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["fbtoken"]:
+            print("Verification fail!")
+            return "Verification token mismatch", 403
+
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
 
 
 @app.route('/', methods=['POST'])
